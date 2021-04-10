3201e2811f4b9b8b3ac66b28b6ea24fd2a7c7317
Matias Puletti <matiaspuletti@Matiass-MacBook-Pro.local>
2021-04-10 11:41:55 +0200

    fix: add missing sidebar routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,9 @@ import Onboarding from './views/Onboarding';
 import Employees from './views/Employees';
 import OrgSettings from './views/OrgSettings';
 import Account from './views/Account';
+import SignUp from './views/SignUp';
+import LogIn from './views/LogIn';
+
 
 function App() {
   return (
@@ -18,6 +21,9 @@ function App() {
           <Route exact path="/onboarding" component={Onboarding} />
           <Route exact path="/corp/settings" component={OrgSettings} />
           <Route exact path="/corp/user/:id" component={Account} />
+          <Route exact path="/auth/sign-up" component={SignUp} />
+          <Route exact path="/auth/log-in" component={LogIn} />
+
         </Switch>
       </Router>
     </>
