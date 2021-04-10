7138fcc940d5ba33dabb2fdd34e209708663ab9d
Matias Puletti <matiaspuletti@Matiass-MacBook-Pro.local>
2021-04-10 16:58:15 +0200

    fix: login auth routes

--- client/src/services/authentication.js
+++ client/src/services/authentication.js
@@ -1,24 +1,24 @@
 import api from './api';
 
 export const signIn = async data => {
-  const response = await api.post('/authentication/signin', data);
+  const response = await api.post('/auth/signin', data);
   const body = response.data;
   const user = body.user;
   return user;
 };
 
 export const signUp = async data => {
-  const response = await api.post('/authentication/signup', data);
+  const response = await api.post('/auth/signup', data);
   const body = response.data;
   const user = body.user;
   return user;
 };
 
 export const signOut = async () => {
-  await api.post('/authentication/signout');
+  await api.post('/auth/signout');
 };
 
 export const verify = async () => {
-  const response = await api.get('/authentication/verify');
+  const response = await api.get('/auth/verify');
   return response.data.user;
 };
