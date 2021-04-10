ceb0adb1315d7adf686e4f3720860dd080cf42ff
Katja Maasch <katja.maasch@gmail.com>
2021-04-10 09:03:49 +0200

    Feat: initial set up for sign-up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import './App.scss';
+import React from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import SignUp from './views/SignUp';
 
 function App() {
-  return <div className="App">enbarque</div>;
+  return (
+    <React.Fragment>
+      <div className="App">enbarque</div>
+      <Router>
+        <Route path="/auth/sign-up" component={SignUp} exact />
+      </Router>
+    </React.Fragment>
+  );
 }
 
 export default App;
