875f085b33b00f89968f9b2e375cdf1e6d15ae30
Katja Maasch <katja.maasch@gmail.com>
2021-04-10 16:26:06 +0200

    style & chore: work on sign-up functionality

--- client/src/views/SignUp.jsx
+++ client/src/views/SignUp.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './../SignUp.scss';
 
 class SignUp extends Component {
   state = {
@@ -10,24 +11,36 @@ class SignUp extends Component {
       city: '',
       country: ''
     },
+    emailCorp: '',
     logo: '',
     admin: {
       firstName: '',
       lastName: ''
     },
+    avatar: '',
     email: '',
     password: ''
   };
 
   handleFormSubmission = async (event) => {
     event.preventDefault();
-    const { name, address, logo, website, admin, email, password } = this.state;
+    const {
+      name,
+      website,
+      emailCorp,
+      address,
+      logo,
+      admin,
+      email,
+      password
+    } = this.state;
     const data = new FormData();
     const values = {
       name,
       address,
       logo,
       website,
+      emailCorp,
       admin,
       email,
       password
@@ -56,114 +69,156 @@ class SignUp extends Component {
     return (
       <div>
         <h1>Register your organization</h1>
-        <h2>Company Info</h2>
         <form onSubmit={this.handleFormSubmission}>
-          <label htmlFor="name-input">Organization</label>
-          <input
-            id="name-input"
-            type="text"
-            placeholder="name of your organization"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleInputChange}
-          />
-          <label htmlFor="website-input">Website</label>
-          <input
-            id="website-input"
-            type="text"
-            placeholder="website of your organization"
-            name="website"
-            value={this.state.website}
-            onChange={this.handleInputChange}
-          />
-          <div>
-            <h4>Address</h4>
-            <label htmlFor="street-input">Street</label>
-            <input
-              id="street-input"
-              type="text"
-              placeholder="address of your organization"
-              name="street"
-              value={this.state.address.street}
-              onChange={this.handleInputChange}
-            />
-            <label htmlFor="postcode-input">Postcode</label>
-            <input
-              id="postcode-input"
-              type="text"
-              placeholder="address of your organization"
-              name="postcode"
-              value={this.state.address.postcode}
-              onChange={this.handleInputChange}
-            />
-            <label htmlFor="city-input">City</label>
-            <input
-              id="city-input"
-              type="text"
-              placeholder="address of your organization"
-              name="city"
-              value={this.state.address.city}
-              onChange={this.handleInputChange}
-            />
-            <label htmlFor="country-input">Country</label>
-            <input
-              id="country-input"
-              type="text"
-              placeholder="address of your organization"
-              name="country"
-              value={this.state.address.country}
-              onChange={this.handleInputChange}
-            />
+          <div className="organisationAndAdminContainer">
+            <div className="company-info">
+              <h2>Company Info</h2>
+              <label htmlFor="name-input">Organization</label>
+              <input
+                id="name-input"
+                type="text"
+                placeholder="name of your organization"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleInputChange}
+                required
+              />
+              <br />
+              <label htmlFor="website-input">Website</label>
+              <input
+                id="website-input"
+                type="text"
+                placeholder="website of your organization"
+                name="website"
+                value={this.state.website}
+                onChange={this.handleInputChange}
+                required
+              />
+              <br />
+              <label htmlFor="emailCorp-input">
+                Default Organization Email
+              </label>
+              <input
+                id="emailCorp-input"
+                type="email"
+                placeholder="mail of your organization"
+                name="emailCorp"
+                value={this.state.mail}
+                onChange={this.handleInputChange}
+                required
+              />
+              <br />
+              <label htmlFor="logo-input">Logo</label>
+              <input
+                id="logo-input"
+                type="file"
+                name="logo"
+                onChange={this.handleFileInputChange}
+              />
+              <div>
+                <h4>Address</h4>
+                <label htmlFor="street-input">Street</label>
+                <input
+                  id="street-input"
+                  type="text"
+                  placeholder="address of your organization"
+                  name="street"
+                  value={this.state.address.street}
+                  onChange={this.handleInputChange}
+                  required
+                />
+                <br />
+                <label htmlFor="postcode-input">Postcode</label>
+                <input
+                  id="postcode-input"
+                  type="text"
+                  placeholder="address of your organization"
+                  name="postcode"
+                  value={this.state.address.postcode}
+                  onChange={this.handleInputChange}
+                  required
+                />
+                <br />
+                <label htmlFor="city-input">City</label>
+                <input
+                  id="city-input"
+                  type="text"
+                  placeholder="address of your organization"
+                  name="city"
+                  value={this.state.address.city}
+                  onChange={this.handleInputChange}
+                  required
+                />
+                <br />
+                <label htmlFor="country-input">Country</label>
+                <input
+                  id="country-input"
+                  type="text"
+                  placeholder="address of your organization"
+                  name="country"
+                  value={this.state.address.country}
+                  onChange={this.handleInputChange}
+                  required
+                />
+                <br />
+              </div>
+            </div>
+            <div>
+              <h2>Administrator Info</h2>
+              <label htmlFor="admin-firstName-input">First Name</label>
+              <input
+                id="admin-firstName-input"
+                type="admin"
+                placeholder="Your first name"
+                name="firstName"
+                value={this.state.admin.firstName}
+                onChange={this.handleInputChange}
+                required
+              />
+              <br />
+              <label htmlFor="admin-lastName-input">Last Name</label>
+              <input
+                id="admin-lastName-input"
+                type="admin"
+                placeholder="Your last name"
+                name="lastName"
+                value={this.state.admin.lastName}
+                onChange={this.handleInputChange}
+                required
+              />
+              <br />
+              <label htmlFor="avatar-input">Avatar</label>
+              <input
+                id="avatar-input"
+                type="file"
+                name="avatar"
+                onChange={this.handleFileInputChange}
+              />
+              <br />
+              <label htmlFor="email-input">Email</label>
+              <input
+                id="email-input"
+                type="email"
+                placeholder="Your email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleInputChange}
+                required
+              />
+              <br />
+              <label htmlFor="password-input">Password</label>
+              <input
+                id="password-input"
+                type="password"
+                placeholder="A secure password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleInputChange}
+                required
+              />
+              <button className="submit">Sign Up</button>
+            </div>
           </div>
-          <div>
-            <label htmlFor="logo-input">Logo</label>
-            <input
-              id="logo-input"
-              type="file"
-              name="logo"
-              onChange={this.handleFileInputChange}
-            />
-          </div>
-          <h2>Administrator Info</h2>
-
-          <label htmlFor="admin-firstName-input">Name</label>
-          <input
-            id="admin-firstName-input"
-            type="admin"
-            placeholder="Your first name"
-            name="firstName"
-            value={this.state.admin.firstName}
-            onChange={this.handleInputChange}
-          />
-          <input
-            id="admin-lastName-input"
-            type="admin"
-            placeholder="Your last name"
-            name="lastName"
-            value={this.state.admin.lastName}
-            onChange={this.handleInputChange}
-          />
-          <label htmlFor="email-input">Email</label>
-          <input
-            id="email-input"
-            type="email"
-            placeholder="Your email"
-            name="email"
-            value={this.state.email}
-            onChange={this.handleInputChange}
-          />
-
-          <label htmlFor="password-input">Password</label>
-          <input
-            id="password-input"
-            type="password"
-            placeholder="A secure password"
-            name="password"
-            value={this.state.password}
-            onChange={this.handleInputChange}
-          />
-
-          <button>Sign Up</button>
         </form>
       </div>
     );
@@ -171,4 +226,3 @@ class SignUp extends Component {
 }
 
 export default SignUp;
-
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -5,58 +5,63 @@ const { Router } = require('express');
 const bcryptjs = require('bcryptjs');
 const User = require('./../models/user.model');
 const Organization = require('./../models/organization.model');
+const fileUploadMiddleware = require('./../middleware/file-upload');
 
 const router = new Router();
 
-router.post('/signup', async (req, res, next) => {
-  const {
-    firstName,
-    lastName,
-    username,
-    email,
-    password,
-    role,
-    level,
-    position
-  } = req.body;
-  let avatar;
-  if (req.body.avatar) {
-    avatar = req.body.avatar;
-  }
-  const { name, adress, emailCorp, website } = req.body;
-  let logo;
-  if (req.body.logo) {
-    logo = req.body.logo;
-  }
-
-  try {
-    const hash = await bcryptjs.hash(password, 10);
-    const company = await Organization.create({
-      name,
-      adress,
-      logo,
-      emailCorp,
-      website
-    });
-    const user = await User.create({
+router.post(
+  '/signup',
+  fileUploadMiddleware.single('avatar'),
+  async (req, res, next) => {
+    const {
       firstName,
       lastName,
       username,
       email,
-      passwordHashAndSalt: hash,
-      organization: company._id,
+      password,
       role,
       level,
-      position,
-      avatar
-    });
-    await Organization.findByIdAndUpdate(company._id, { admin: user._id });
-    req.session.userId = user._id;
-    res.json({ user });
-  } catch (error) {
-    next(error);
+      position
+    } = req.body;
+    let avatar;
+    if (req.body.avatar) {
+      avatar = req.body.avatar;
+    }
+    const { name, adress, emailCorp, website } = req.body;
+    let logo;
+    if (req.body.logo) {
+      logo = req.body.logo;
+    }
+
+    try {
+      const hash = await bcryptjs.hash(password, 10);
+      const company = await Organization.create({
+        name,
+        adress,
+        logo,
+        emailCorp,
+        website
+      });
+      const user = await User.create({
+        firstName,
+        lastName,
+        username,
+        email,
+        passwordHashAndSalt: hash,
+        organization: company._id,
+        role,
+        level,
+        position,
+        avatar
+      });
+      await Organization.findByIdAndUpdate(company._id, { admin: user._id });
+      req.session.userId = user._id;
+      res.json({ user });
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
 router.post('/signin', (req, res, next) => {
   let user;
