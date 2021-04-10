8af64fbc025f4b97a8531bb90ad12856ba7cede3
Victor Nastasa <victornast@gmail.com>
2021-04-10 14:57:24 +0200

    feat: update signup functionality

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -4,38 +4,58 @@ const { Router } = require('express');
 
 const bcryptjs = require('bcryptjs');
 const User = require('../models/user.model');
+const Organization = require('../models/organization.model');
 
 const router = new Router();
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup', async (req, res, next) => {
   const {
     firstName,
     lastName,
+    username,
     email,
     password,
     role,
     level,
-    position,
-    avatar,
-    organization
+    position
   } = req.body;
-  const { name, adress, logo, admin, emailCorp, website } = req.body;
-  bcryptjs
-    .hash(password, 10)
-    .then((hash) => {
-      return User.create({
-        name,
-        email,
-        passwordHashAndSalt: hash
-      });
-    })
-    .then((user) => {
-      req.session.userId = user._id;
-      res.json({ user });
-    })
-    .catch((error) => {
-      next(error);
+  let avatar;
+  if (req.body.avatar) {
+    avatar = req.body.avatar;
+  }
+  const { name, adress, emailCorp, website } = req.body;
+  let logo;
+  if (req.body.logo) {
+    logo = req.body.logo;
+  }
+
+  try {
+    const hash = await bcryptjs.hash(password, 10);
+    const company = await Organization.create({
+      name,
+      adress,
+      logo,
+      emailCorp,
+      website
     });
+    const user = await User.create({
+      firstName,
+      lastName,
+      username,
+      email,
+      passwordHashAndSalt: hash,
+      organization: company._id,
+      role,
+      level,
+      position,
+      avatar
+    });
+    await Organization.findByIdAndUpdate(company._id, { admin: user._id });
+    req.session.userId = user._id;
+    res.json({ user });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post('/signin', (req, res, next) => {
