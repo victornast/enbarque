f3aa0d2cedff178824983f902178a02d7bfccf43
Victor Nastasa <victornast@gmail.com>
2021-04-12 20:53:01 +0200

    chore: remove unused vars

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,19 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import React, { useState } from "react";
-import "./App.scss";
-import Navbar from "./components/Navbar/Navbar";
-import Dashboard from "./views/Dashboard";
-import Onboarding from "./views/Onboarding";
-import Employees from "./views/Employees";
-import OrgSettings from "./views/OrgSettings";
-import Account from "./views/Account";
-import SignUp from "./views/SignUp";
-import LogIn from "./views/LogIn";
-import CreateTask from "./views/CreateTask";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import React, { useState } from 'react';
+import './App.scss';
+import Navbar from './components/Navbar/Navbar';
+import Dashboard from './views/Dashboard';
+import Onboarding from './views/Onboarding';
+import Employees from './views/Employees';
+import OrgSettings from './views/OrgSettings';
+import Account from './views/Account';
+import SignUp from './views/SignUp';
+import LogIn from './views/LogIn';
+import CreateTask from './views/CreateTask';
 
 function App() {
   const [user, setUser] = useState(null);
@@ -22,7 +26,17 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route exact path="/dashboard" component={Dashboard} />
+          <Route
+            exact
+            path="/dashboard"
+            render={(props) => (
+              <Dashboard
+                user={user}
+                {...props}
+                onUserChange={handleUserChange}
+              />
+            )}
+          />
           <Route exact path="/user/team" component={Employees} />
           <Route exact path="/onboarding" component={Onboarding} />
           <Route exact path="/corp/settings" component={OrgSettings} />
