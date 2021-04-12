3b6a5983ba045a234632708874bba79dfab22cc7
Victor Nastasa <victornast@gmail.com>
2021-04-12 20:59:39 +0200

    feat: add user reference on dashboard

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-function Dashboard() {
-    return (
-        <div className='dashboard'>
-            <h1>Dashboard</h1>
-        </div>
-    )
+function Dashboard({ user }) {
+  console.log(user);
+  return (
+    <div className="dashboard">
+      <h1>Dashboard</h1>
+      <div>
+        <img
+          src={user.avatar}
+          alt={user.firstName + ' ' + user.lastName}
+          height={50}
+        />
+        <span>Hello, {user.firstName}.</span>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
