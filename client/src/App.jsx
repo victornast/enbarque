e26854f0f50294d8cc72d7992d084acd9517a8ae
Harumi Terayama <hterayama@gmail.com>
2021-04-13 19:58:31 +0200

    feat: protected route for the dashboard is working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,24 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import ProtectedRoute from './components/ProtectedRoute';
-import { signOut, verify } from './services/authentication';
-import React, { Component, useState } from 'react';
-import './App.scss';
-import Navbar from './components/Navbar/Navbar';
-import Dashboard from './views/Dashboard';
-import Onboarding from './views/Onboarding';
-import Employees from './views/Employees';
-import OrgSettings from './views/OrgSettings';
-import Account from './views/Account';
-import SignUp from './views/SignUp';
-import LogIn from './views/LogIn';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { signOut, verify } from "./services/authentication";
+import React, { Component, useState } from "react";
+import "./App.scss";
+import Navbar from "./components/Navbar/Navbar";
+import Dashboard from "./views/Dashboard";
+import Onboarding from "./views/Onboarding";
+import Employees from "./views/Employees";
+import OrgSettings from "./views/OrgSettings";
+import Account from "./views/Account";
+import SignUp from "./views/SignUp";
+import LogIn from "./views/LogIn";
 // added in the branch to test
-import AddUser from './views/AddUser';
-import CreateTask from './views/CreateTask';
+import AddUser from "./views/AddUser";
+import CreateTask from "./views/CreateTask";
 
 class App extends Component {
   state = {
     user: null,
-    loaded: false
+    loaded: false,
   };
 
   async componentDidMount() {
@@ -29,7 +29,7 @@ class App extends Component {
 
   handleUserChange = (user) => {
     this.setState({
-      user
+      user,
     });
   };
 
@@ -47,14 +47,11 @@ class App extends Component {
             <ProtectedRoute
               exact
               path="/dashboard"
-              render={(props) => (
-                <Dashboard
-                  {...props}
-                  user={this.state.user}
-                  authorized={this.state.user}
-                />
-              )}
+              authorized={this.state.user}
               redirect="/auth/signin"
+              render={(props) => (
+                <Dashboard {...props} user={this.state.user} />
+              )}
             />
             <Route exact path="/user/team" component={Employees} />
             <Route exact path="/onboarding" component={Onboarding} />
@@ -76,7 +73,7 @@ class App extends Component {
             />
             <Route
               exact
-              path="/auth/login"
+              path="/auth/signin"
               render={(props) => (
                 <LogIn {...props} onUserChange={this.handleUserChange} />
               )}
