682fa2843e3dbf6c31b931124c50d26e80b8702c
Matias Puletti <matiaspuletti@Matiass-MacBook-Pro.local>
2021-04-14 21:15:54 +0200

    fix: display all user information

--- client/src/views/Account.jsx
+++ client/src/views/Account.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Component } from 'react';
 import { loadUser } from '../services/user';
+import './../Account.scss';
+import { Divider, Avatar } from '@material-ui/core';
 
 class Account extends Component {
   state = {
@@ -18,9 +20,45 @@ class Account extends Component {
       <main>
         {user && (
           <>
-            <h1>{user.firstName}</h1>
-            <h1>{user.lastName}</h1>
+            <div>
+              <div className="align">
+                 <h1 className="margin">{user.firstName}'s profile </h1>
+              <Avatar src={user.avatar}></Avatar>
+              </div>
+             
+              <Divider />
 
+              <div className="row">
+                <div className="column">
+                  {' '}
+                  <p>
+                    <span className="text">Full name: </span> {user.firstName}{' '}
+                    {user.lastName}
+                  </p>
+                  <p>
+                    <span className="text">Email: </span> {user.email}
+                  </p>
+                  <p>
+                    <span className="text">Organization:</span>{' '}
+                    {user.organization}
+                  </p>
+                </div>
+                <div className="column">
+                  {' '}
+                  <p>
+                    {' '}
+                    <span className="text">Position: </span> {user.position}
+                  </p>
+                  <p>
+                    <span className="text">Level: </span> {user.level}
+                  </p>
+                  <p>
+                    {' '}
+                    <span className="text">Role: </span> {user.role}
+                  </p>
+                </div>
+              </div>
+            </div>
           </>
         )}
       </main>
