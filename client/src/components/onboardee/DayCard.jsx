5449ba7030d8f7124baff8bc45a5f2f73432e6f2
Harumi Terayama <hterayama@gmail.com>
2021-04-17 22:43:17 +0200

    fix: starting day + nDay - 1

--- client/src/components/onboardee/DayCard.jsx
+++ client/src/components/onboardee/DayCard.jsx
@@ -4,11 +4,8 @@ import "./Schedule.scss";
 export class DayCard extends Component {
   render() {
     const startDate = new Date(this.props.startDate);
-    console.log(startDate);
-    const date = startDate.setDate(startDate.getDate() + this.props.nDay);
+    const date = startDate.setDate(startDate.getDate() + this.props.nDay - 1);
     const displayDate = new Date(date).toDateString();
-    console.log(date);
-    console.log(displayDate);
     return (
       <div className="day-card">
         <h2>Day {this.props.nDay}</h2>
