889ad4c4bf416b68eed87dfac4fdfc7ea009aba3
Harumi Terayama <hterayama@gmail.com>
2021-04-19 00:00:40 +0200

    feat: added link to the scheduled task to see the details of the task

--- client/src/components/onboardee/ScheduledTaskCard.jsx
+++ client/src/components/onboardee/ScheduledTaskCard.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ScheduledTaskCard = (task) => {
   console.log(task);
   return (
     <div>
-      <h5>{task.task.task.headline}</h5>
+      <Link to={`/tasks/${task.task.task._id}`}>
+        <h5>{task.task.task.headline}</h5>
+      </Link>
     </div>
   );
 };
