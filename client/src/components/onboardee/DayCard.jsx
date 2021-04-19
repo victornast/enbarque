927e3868632b7680f3a12d83f1922469f5e59f77
Harumi Terayama <hterayama@gmail.com>
2021-04-19 20:02:25 +0200

    feat: Not displaying year in the schedule. Applied basic styling.

--- client/src/components/onboardee/DayCard.jsx
+++ client/src/components/onboardee/DayCard.jsx
@@ -7,7 +7,7 @@ export class DayCard extends Component {
     const scheduledTasks = this.props.process.scheduledTasks;
     const startDate = new Date(this.props.startDate);
     const date = startDate.setDate(startDate.getDate() + this.props.nDay - 1);
-    const displayDate = new Date(date).toDateString();
+    const displayDate = new Date(date).toDateString().slice(0, -4);
     const taskOfTheDay = scheduledTasks.filter(
       (task) =>
         new Date(task.startingTimeSlot).getDate() === new Date(date).getDate()
@@ -15,7 +15,7 @@ export class DayCard extends Component {
     console.log(taskOfTheDay);
     return (
       <div className="day-card">
-        <h2 className="day-card__OBday">Day {this.props.nDay}</h2>
+        <h4 className="day-card__OBday">Day {this.props.nDay}</h4>
         <h3 className="day-card__date">{displayDate}</h3>
         <div className="day-card__tasks">
           {scheduledTasks &&
