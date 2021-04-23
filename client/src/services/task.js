325bacf31a4274d8dcfb7d9a3ce90c889c8938ab
Victor Nastasa <victornast@gmail.com>
2021-04-23 22:39:09 +0200

    feat: style task list

--- client/src/services/task.js
+++ client/src/services/task.js
@@ -1,25 +1,19 @@
-import api from "./api";
+import api from './api';
 
 export const createTask = async (data) => {
-  console.log("data: ", data);
-  const response = await api.post("/tasks/create", data);
-  console.log("response: ", response);
+  const response = await api.post('/tasks/create', data);
   const body = response.data;
   const createdTask = body.createdTask;
   return createdTask;
 };
 
 export const updateTask = async (id, data) => {
-  console.log("data: ", data);
-  console.log("id: ", id);
   const response = await api.patch(`/tasks/${id}/edit`, data);
   return response;
 };
 
 export const findTasks = async () => {
-  const response = await api.get("/tasks");
-
-  console.log("response.data: ", response.data);
+  const response = await api.get('/tasks');
   return response.data.allTasks;
 };
 
@@ -29,9 +23,7 @@ export const loadTask = async (id) => {
   return task;
 };
 
-//is this correct?
 export const deleteTask = async (id) => {
-  console.log("api endpoint for delete task has been called");
   const response = await api.delete(`/tasks/${id}/delete`);
   return response;
 };
