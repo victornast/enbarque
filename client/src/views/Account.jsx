0294ff43fb489607fcdcd6d8aefbb625f3e874d7
Victor Nastasa <victornast@gmail.com>
2021-04-23 23:59:55 +0200

    feat: style account

--- client/src/views/Account.jsx
+++ client/src/views/Account.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
 import { loadUser } from '../services/user';
-import './../Account.scss';
+import './Account.scss';
 import { Divider, Avatar } from '@material-ui/core';
 
 class Account extends Component {
@@ -11,60 +11,45 @@ class Account extends Component {
 
   async componentDidMount() {
     const user = await loadUser(this.props.match.params.id);
-    console.log(user);
     this.setState({ user });
   }
 
   render() {
     const { user } = this.state;
     return (
-      <main>
-        {user && (
-          <>
-            <div>
-              <div className="align">
-                <h1 className="margin">{user.firstName}'s profile </h1>
-                <Avatar src={user.avatar}></Avatar>
-              </div>
+      user && (
+        <article>
+          <div className="align">
+            <h1 className="margin">
+              {user.firstName + ' ' + user.lastName}'s profile{' '}
+            </h1>
+            <Avatar src={user.avatar}></Avatar>
+          </div>
 
-              <Divider />
-
-              <div className="row">
-                <div className="column">
-                  {' '}
-                  <p>
-                    <span className="text">Full name: </span> {user.firstName}{' '}
-                    {user.lastName}
-                  </p>
-                  <p>
-                    <span className="text">Email: </span> {user.email}
-                  </p>
-                  <p>
-                    <span className="text">Organization:</span>{' '}
-                    {user.organization.name}
-                  </p>
-                </div>
-                <div className="column">
-                  {' '}
-                  <p>
-                    {' '}
-                    <span className="text">Position: </span>{' '}
-                    {user.position.name}
-                  </p>
-                  <p>
-                    <span className="text">Level: </span> {user.level.name}
-                  </p>
-                  <p>
-                    {' '}
-                    <span className="text">Role: </span> {user.role.name}
-                  </p>
-                  <br />
-                </div>
-              </div>
-            </div>
-          </>
-        )}
-      </main>
+          <Divider />
+          <ul className="eb-account__details">
+            <li>
+              <span className="text">Full name: </span>
+              {user.firstName} {user.lastName}
+            </li>
+            <li>
+              <span className="text">Email: </span> {user.email}
+            </li>
+            <li>
+              <span className="text">Position: </span>
+              {user.position.name}
+            </li>
+            <li>
+              <span className="text">Level: </span>
+              {user.level.name}
+            </li>
+            <li>
+              <span className="text">Role: </span>
+              {user.role.name}
+            </li>
+          </ul>
+        </article>
+      )
     );
   }
 }
